8b3f8aacf0eadd6eeed0fdccd83725c9ceb28eb1
heweizhi <heweizhi@guanmai.cn>
2021-08-24 15:24:20 +0800

    WIP: test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,3 @@
-// const path = require('path')
-
 module.exports = {
   extends: ['plugin:gm-react-app/recommended'],
-  // settings: {
-  //   'import/resolver': {
-  //     // 配置 alias,和 webpack config.resolver.alias 保持一致即可
-  //     'gm-alias': {
-  //       '@': path.resolve(__dirname, 'src/'),
-  //       react: path.resolve(__dirname, '..', 'node_modules/react'),
-  //     },
-  //   },
-  // },
 }
